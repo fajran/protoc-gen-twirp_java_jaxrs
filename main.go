2c73c6d14eca522e913383bf85b295aa0ae637a8
Fajran Iman Rusadi <fajran@gmail.com>
2018-01-23 22:06:07 +0100

    Add JAX-RS Client generator

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"os"
 
+	"github.com/golang/protobuf/proto"
+	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+)
+
+func main() {
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic("error reading input")
+	}
+
+	req := &plugin.CodeGeneratorRequest{}
+	err = proto.Unmarshal(data, req)
+	if err != nil {
+		panic("error parsing input proto")
+	}
+
+	g := newGenerator(req)
+
+	err = g.Generate()
+	if err != nil {
+		panic("error generating output")
+	}
+
+	data, err = proto.Marshal(g.Response)
+	if err != nil {
+		panic("error marshaling output proto")
+	}
+
+	_, err = os.Stdout.Write(data)
+	if err != nil {
+		panic("error writing output")
+	}
 }
---
+++ utils.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"unicode"
+
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+)
+
+const (
+	javaOuterClassSuffix = "OuterClass"
+)
+
+func getProtoName(file *descriptor.FileDescriptorProto) string {
+	name := file.GetName()
+	ext := filepath.Ext(name)
+	if ext == ".proto" || ext == ".protodevel" {
+		name = name[0 : len(name)-len(ext)]
+	}
+	return name
+}
+
+func getJavaOuterClassName(file *descriptor.FileDescriptorProto) string {
+	name := file.Options.GetJavaOuterClassname()
+	if name != "" {
+		return name
+	}
+
+	name = camelCase(getProtoName(file))
+	outer := name + javaOuterClassSuffix
+	for _, desc := range file.GetMessageType() {
+		if strings.Title(desc.GetName()) == name {
+			return outer
+		}
+	}
+
+	for _, desc := range file.GetService() {
+		if strings.Title(desc.GetName()) == name {
+			return outer
+		}
+	}
+
+	for _, desc := range file.GetEnumType() {
+		if strings.Title(desc.GetName()) == name {
+			return outer
+		}
+	}
+
+	return name
+}
+
+func getJavaServiceClientClassName(file *descriptor.FileDescriptorProto, service *descriptor.ServiceDescriptorProto) string {
+	outerClass := getJavaOuterClassName(file)
+	serviceName := camelCase(service.GetName())
+	return fmt.Sprintf("%s_%sClient", outerClass, serviceName)
+}
+
+func getJavaServiceClientClassFile(file *descriptor.FileDescriptorProto, service *descriptor.ServiceDescriptorProto) string {
+	serviceClass := getJavaServiceClientClassName(file, service)
+	pkg := getJavaPackage(file)
+	dir := strings.Replace(pkg, ".", "/", -1)
+	return fmt.Sprintf("%s/%s.java", dir, serviceClass)
+}
+
+func getJavaPackage(file *descriptor.FileDescriptorProto) string {
+	pkg := file.Options.GetJavaPackage()
+	if pkg != "" {
+		return pkg
+	}
+	return file.GetPackage()
+}
+
+func getJavaType(file *descriptor.FileDescriptorProto, name string) string {
+	pkg := getJavaPackage(file)
+	outerClass := getJavaOuterClassName(file)
+
+	p := strings.LastIndex(name, ".")
+	typeName := name[p+1:]
+
+	return fmt.Sprintf("%s.%s.%s", pkg, outerClass, typeName)
+}
+
+func camelCase(str string) string {
+	parts := strings.Split(str, "_")
+	for i, part := range parts {
+		runes := []rune(part)
+		runes[0] = unicode.ToUpper(runes[0])
+		parts[i] = string(runes)
+	}
+	return strings.Join(parts, "")
+}
+
+func lowerCamelCase(str string) string {
+	cc := camelCase(str)
+	runes := []rune(cc)
+	runes[0] = unicode.ToLower(runes[0])
+	return string(runes)
+}
